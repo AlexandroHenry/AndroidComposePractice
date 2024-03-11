cda42f7eb06ee6c00f770862324d731f6b86c39a
AlexandroHenry <alaxhenry0121@gmail.com>
2024-03-11 16:22:05 +0900

    안드로이드 연습용 레포지토리

---
+++ app/src/main/java/com/example/composepractice1/di/NetworkModule.kt
@@ -0,0 +1,6 @@
+package com.example.composepractice1.di
+
+
+class NetworkModule {
+
+}
---
+++ app/src/main/java/com/example/composepractice1/util/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.composepractice1.util
+
+const val HOME = "home"
+const val CHAT = "chat"
+const val SETTINGS = "settings"
